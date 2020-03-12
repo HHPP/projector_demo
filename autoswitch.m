4a6fa7c9c659ec9f95ab5a63e81ae97c671fb7c5
DJHansford <david.j.hansford@gmail.com>
2020-03-12 16:41:30 +0000

    added time limit

--- autoswitch.m
+++ autoswitch.m
@@ -1,12 +1,12 @@
 function[] = autoswitch(delay, time)
 % AUTOSWITCH connects to a Tektronix AFG3022 Signal Generator to apply the
 %   square wave for peak performance of the cell DJH-151118-01 190329-1 at
-%   55°C. It switches this field on and off every 'delay' seconds for
-%   'time' seconds.
+%   55°C. It switches this field on and off every 'delay' seconds for a
+%   total of 'time' seconds.
 %
-%   AUTOSWITCH(1)
+%   AUTOSWITCH(1,6)
 %
-%   Delay is set to 1 second
+%   Delay is set to 1 second, total time is 6 seconds
 
 instrreset;
 param.afg=visa('ni','USB::0x0699::0x0341::C020167::INSTR');
